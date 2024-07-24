5882335b7bf0d822d3220584fee0927f6500a969
Belbareiro <mbareiromereles@gmail.com>
2024-07-24 12:47:04 -0400

    Cliente

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductForm from './components/ProductForm';
+import '../src/App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Administrador de Productos</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../components/ProductForm.css'
+
+const ProductForm = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/api/products', {
+                title,
+                price,
+                description
+            });
+            console.log('Producto agregado:', response.data);
+            // Reiniciar el formulario
+            setTitle('');
+            setPrice('');
+            setDescription('');
+        } catch (error) {
+            console.error('Error al agregar el producto:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Título"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+            />
+            <input
+                type="number"
+                placeholder="Precio"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required
+            />
+            <textarea
+                placeholder="Descripción"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+            />
+            <button type="submit">Agregar Producto</button>
+        </form>
+    );
+};
+
+export default ProductForm;
